bbb21a3ed069b2ef526c9e7592eb050a75487cc5
moonfarzana <moonfarzana007@gmail.com>
2025-08-04 21:38:16 +0600

    04:Backgroundjob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import { getQueryClient,  trpc } from '@/trpc/server';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import Client from './client';
-import { Suspense } from 'react';
+'use client'
+import { Button } from '@/components/ui/button'
+import { useTRPC } from '@/trpc/client'
+import { useMutation } from '@tanstack/react-query'
+import React from 'react'
 
-export default async function Home() {
-  const queryClient = getQueryClient();
- void queryClient.prefetchQuery(trpc.createAI.queryOptions({text: "Moon"}));
-  // Do something with greeting on the server
+const Page = () => {
+ const trpc =useTRPC();
+ const invoke =useMutation(trpc.invoke.mutationOptions({}))
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-
- <Client />
-      </Suspense>
-    </HydrationBoundary>
-  );
+    <div className='p-4 max-w-3xl mx-auto'>
+      <Button onClick={() => invoke.mutate({text:"Test"})}>
+        Invoke background job
+      </Button>
+    </div>
+  )
 }
+
+export default Page
